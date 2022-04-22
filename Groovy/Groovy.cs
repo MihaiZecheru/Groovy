46a20e4153957d5d3236ec66c72a4a00e0a7c812
MihaiZecheru <zecheruchris@gmail.com>
2022-04-22 13:47:49 -0700

    Revert "Revert "developing still in progress. Added a music queue, switched to using NAudio, added queue and "all" playlist, refactored some code, and more.""
    This reverts commit e43a61cd79e95c1d0403a2674ddaa00dd86f0375.

--- Groovy/Groovy.cs
+++ Groovy/Groovy.cs
@@ -1,4 +1,6 @@
 using ChrisTools;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 
 namespace Groovy
 {
@@ -8,22 +10,28 @@ namespace Groovy
         /// Executes: Messagebox.Show(x); where x is any type
         /// </summary>
         /// <param name="x">The thing you want to be displayed on the screen; can be of any type</param>
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
         public static void display(object x, object y = null, object z = null, object a = null, object b = null)
         {
-#pragma warning disable CS8601 // Possible null reference assignment.
-            string[] args = { x != null ? x.ToString(): null, y != null? y.ToString() : null, z != null ? z.ToString() : null, a != null ? a.ToString() : null, b != null ? b.ToString() : null };
-#pragma warning restore CS8601 // Possible null reference assignment.
+            string[] args = { x != null ? x.ToString(): null, y != null ? y.ToString() : null, z != null ? z.ToString() : null, a != null ? a.ToString() : null, b != null ? b.ToString() : null };
             string toShow = "";
             foreach (string arg in args)
             {
                 if (arg != null)
+                {
+                    if (arg.GetType().IsArray)
+                    {
+                        foreach (object ele in arg)
+                            toShow += (toShow != "") ? ", " + ele : ele;
+                    }
                     toShow += (toShow != "") ? ", " + arg : arg;
+                }
             }
             MessageBox.Show(toShow);
         }
-#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
 
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        private List<string> MusicQueue = new List<string>();
 
         public Groovy()
         {
@@ -32,6 +40,7 @@ namespace Groovy
             this.playlists_list.Items.Clear();
             this.songs_list.Items.Clear();
             PopulatePlaylistsListbox();
+            DisplayFirstPlaylist();
         }
 
         private List<string> all_playlists = new List<string>();
@@ -45,6 +54,10 @@ namespace Groovy
 
             this.playlists_list.Items.Add("all");
             this.all_playlists.Add("all");
+
+            /* add queue, this is not a playlist */
+
+            this.playlists_list.Items.Add("queue");
 
             /* find and add <char>:\Music (C:\Music, D:\Music, etc)*/
 
@@ -95,7 +108,7 @@ namespace Groovy
             }
         }
 
-        private int previouslySelectedPlaylistIndex = -2; // default number that will not be an index in the listbox
+        private int previouslySelectedPlaylistIndex = 0; // default playlist is always the first in the users line
         private void playlists_list_SelectedIndexChanged(object sender, EventArgs e)
         {
             try
@@ -104,9 +117,7 @@ namespace Groovy
                 if (previouslySelectedPlaylistIndex == currentlySelectedIndex)
                     return; // only activate this function when a new playlist is clicked
 
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-                string playlist = this.playlists_list.SelectedItems[0].ToString();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                string playlist = (string)this.playlists_list.SelectedItems[0];
                 previouslySelectedPlaylistIndex = currentlySelectedIndex;
                 this.playlists_list.ClearSelected();
                 this.ActiveControl = this.icon_and_name_display;
@@ -119,10 +130,11 @@ namespace Groovy
 
         private void ShowPlaylistSongs(string playlist)
         {
-            playlist = playlist.Contains(":\\") ? musicfilePath : (playlist == "all") ? playlist : @$"C:\groovy\{playlist}";
+            playlist = playlist.Contains(":\\") ? musicfilePath : (playlist == "all" || playlist == "queue") ? playlist : @$"C:\groovy\{playlist}";
             List<string> songs = new List<string>();
-            if (playlist != "all")
+            if (playlist != "all" && playlist != "queue")
             {
+                this.songs_list.Sorted = true;
                 foreach (string s in Directory.GetFiles(playlist, "*.mp3"))
                 {
                     string so = s.Substring(s.LastIndexOf('\\') + 1, s.Substring(s.LastIndexOf('\\') + 1).Length - 4);
@@ -130,8 +142,9 @@ namespace Groovy
                         songs.Add(so);
                 }
             }
-            else
+            else if (playlist == "all")
             {
+                this.songs_list.Sorted = true;
                 for (int i = 0; i < this.all_playlists.Count; i++)
                 {
                     string pl = this.all_playlists[i];
@@ -145,6 +158,14 @@ namespace Groovy
                     }
                 }
             }
+            else if (playlist == "queue")
+            {
+                this.songs_list.Sorted = false;
+                foreach (string song in MusicQueue)
+                {
+                    songs.Add(song.Substring(song.LastIndexOf('\\') + 1, song.Substring(song.LastIndexOf('\\') + 1).Length - 4));
+                }
+            }
 
             this.songs_list.Items.Clear();
             foreach (string s in songs)
@@ -152,56 +173,77 @@ namespace Groovy
                 // append each song, removing the leading parent dirs and .mp3 trail
                 this.songs_list.Items.Add(s);
             }
+
+            if (playlist != "queue")
+                filter_songs_textbox_TextChanged(null, null);
         }
 
-        private int previouslySelectedSongIndex = -2;
-        private int indexOfPlaylistWhenLastSongWasPlayed = -2;
         private void songs_list_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (previouslySelectedPlaylistIndex == -2)
-                return; // no playlist has been selected, therefore no songs could have been chosen
+            if (current_playlist_display.Text == "queue")
+            {
+                this.songs_list.ClearSelected();
+                this.ActiveControl = this.icon_and_name_display;
+                return;
+            }
+            //if (previouslySelectedPlaylistIndex == -2)
+            //    return; // no playlist has been selected, therefore no songs could have been chosen
 
             int currentlySelectedSongIndex = this.songs_list.SelectedIndex;
 
-            if (currentlySelectedSongIndex == previouslySelectedSongIndex && indexOfPlaylistWhenLastSongWasPlayed == previouslySelectedPlaylistIndex)
-                return; // the song should only play if the index is the same and the playlists are different
+            //if (currentlySelectedSongIndex == previouslySelectedSongIndex && indexOfPlaylistWhenLastSongWasPlayed == previouslySelectedPlaylistIndex)
+            //    return; // the song should only play if the index is the same and the playlists are different
 
             try
             {
-                string song = this.songs_list.SelectedItems[0].ToString();
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-                string playlist = this.playlists_list.Items[previouslySelectedPlaylistIndex].ToString();
+                string song = (string)this.songs_list.SelectedItems[0];
+                string playlist = (string)this.playlists_list.Items[previouslySelectedPlaylistIndex];
                 playlist = (playlist == "all") ? findPlaylist(song) : playlist;
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.                  start "" "C:\file\path\of\song.mp3"
-                string playSongCommand = playlist.Contains(":\\") ? $"start \"\" \"{musicfilePath}\\{song}.mp3\"" : $"start \"\" \"C:\\groovy\\{playlist}\\{song}.mp3\"";
-                previouslyPlayedSongCommand = playSongCommand;
-                previouslySelectedSongIndex = currentlySelectedSongIndex;
-                indexOfPlaylistWhenLastSongWasPlayed = previouslySelectedPlaylistIndex;
+                string playSongCommand = playlist.Contains(":\\") ? $"{musicfilePath}\\{song}.mp3" : $@"C:\groovy\{playlist}\{song}.mp3";
+                previouslyPlayedSongFilepath = playSongCommand;
+                //previouslySelectedSongIndex = currentlySelectedSongIndex;
                 this.songs_list.ClearSelected();
                 this.ActiveControl = this.icon_and_name_display;
-                playSong(playSongCommand);
+                addSongToQueue(playSongCommand);
+                playSong();
             } catch (IndexOutOfRangeException) { };
         }
 
-        private string previouslyPlayedSongCommand = "{empty}";
-        private void playSong(string command)
+        private string previouslyPlayedSongFilepath = "{empty}";
+        private void playSong()
         {
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfo.FileName = "cmd.exe";
-            startInfo.Arguments = "/C " + command;
-            process.StartInfo = startInfo;
-            process.Start();
-        }
+            if (outputDevice?.PlaybackState == PlaybackState.Playing || outputDevice?.PlaybackState == PlaybackState.Paused)
+                return;
 
+            // play music
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += this.OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                if (MusicQueue.Count > 0)
+                {
+                    audioFile = new AudioFileReader(MusicQueue[0]);
+                    outputDevice.Init(audioFile);
+                }
+                else
+                    return; // no music is left in the queue
+            }
+            // remove song from queue
+            MusicQueue.RemoveAt(0);
+            outputDevice.Play();
+        }
 
         private void replaySong_button_Click(object sender, EventArgs e)
         {
-            if (previouslyPlayedSongCommand == "{empty}")
+            if (previouslyPlayedSongFilepath == "{empty}")
                 return;
 
-            playSong(previouslyPlayedSongCommand);
+            // add to beginning of queue
+            MusicQueue.Insert(0, previouslyPlayedSongFilepath);
+            playSong();
         }
 
         private string findPlaylist(string song)
@@ -219,7 +261,77 @@ namespace Groovy
                     }
                 }
             }
-            return null; // this will never be reached
+            return null; // prevent none-returned exception (CS0161)
+        }
+
+        private List<string> temporarilyRemovedSongs = new List<string>();
+        private int previousTextLength = 0;
+        private void filter_songs_textbox_TextChanged(object sender, EventArgs e)
+        {
+            List<string> toRemove = new List<string>();
+            string text = Tools.Strip(this.filter_music_textbox.Text.ToLower());
+
+            if (text.Length >= this.previousTextLength)
+            {
+                // a char was added, remove songs that do not start with the search-phrase
+                foreach (string song in this.songs_list.Items)
+                {
+                    if (!song.ToLower().Contains(text))
+                    {
+                        this.temporarilyRemovedSongs.Add(song);
+                        toRemove.Add(song);
+                    }
+                }
+
+                foreach (string song in toRemove)
+                {
+                    this.songs_list.Items.Remove(song);
+                }
+            }
+            else
+            {
+                // a char was removed; look through removed songs to add back
+                foreach (string song in this.temporarilyRemovedSongs)
+                {
+                    if (song.Contains(text) && !this.songs_list.Items.Contains(song))
+                        this.songs_list.Items.Add(song);
+                }
+            }
+            previousTextLength = text.Length;
+        }
+
+        private void DisplayFirstPlaylist()
+        {
+            string playlist = (string)this.playlists_list.Items[0];
+            this.playlists_list.ClearSelected();
+            this.ActiveControl = this.icon_and_name_display;
+            this.current_playlist_display.Text = playlist;
+            this.current_playlist_display.Location = new Point((this.songs_list.Width - this.current_playlist_display.Width) / 2 + this.songs_list.Location.X, this.current_playlist_display.Location.Y);
+            ShowPlaylistSongs(playlist);
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            // stop playlist music
+            outputDevice?.Stop();
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            try
+            {
+                outputDevice.Dispose();
+                outputDevice = null;
+                audioFile.Dispose();
+                audioFile = null;
+            }
+            catch (NullReferenceException) { };
+            playSong();
+        }
+
+        private void addSongToQueue(string path)
+        {
+            MusicQueue.Add(path);
         }
     }
 }
